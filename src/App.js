7bdaae47239567df3bf60a6bf7c4bdb4389aad19
adritadeb <adritad2019@gmail.com>
2022-04-04 00:41:46 +0600

    header-done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/reviews' element={<Home></Home>}></Route>
+        <Route path='/dashboard' element={<Home></Home>}></Route>
+        <Route path='/blogs' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <nav className='mt-8'>
+                <Link className='mx-6 hover:bg-red-400 hover:text-white rounded-md p-3' to='/home'>HOME</Link>
+                <Link className='mx-6 hover:bg-red-400 hover:text-white rounded-md p-3' to='/reviews'>REVIEWS</Link>
+                <Link className='mx-6 hover:bg-red-400 hover:text-white rounded-md p-4' to='/dashboard'>DASHBOARD</Link>
+                <Link className='mx-6 hover:bg-red-400 hover:text-white rounded-md p-3' to='/blogs'>BLOGS</Link>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
