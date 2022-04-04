909afde823b7955a94e795cb80a3d6268f6c6dcf
adritadeb <adritad2019@gmail.com>
2022-04-04 19:01:24 +0600

    first-QnA-done

---
+++ src/components/BlogAns1/BlogAns1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BlogAns1 = () => {
+    return (
+        <div>
+            <h2>1. What is Context API?</h2>
+            <p>-- Context API is a component structure of React. The purpose of the context API is to pass data to any level of an application. It helps us to solve prop drilling. If we use context API, we don't need to share data by props at every level to send data to a particular component. We can directly send data from the grandparent component to the child component and others. We can pass many kinds of values with it. Like - state, string, array, dynamic value, etc.</p>
+        </div>
+    );
+};
+
+export default BlogAns1;
---
+++ src/components/Blogs/Blogs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BlogAns1 from '../BlogAns1/BlogAns1';
+
+const Blogs = () => {
+    return (
+        <div className='text-left m-16'>
+            <h1>Blog Section</h1>
+            <BlogAns1></BlogAns1>
+        </div>
+    );
+};
+
+export default Blogs;
