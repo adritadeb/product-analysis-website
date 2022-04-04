e27edd21a189124a8927adcc2f2e99c4ce476c4f
adritadeb <adritad2019@gmail.com>
2022-04-04 13:54:05 +0600

    custom-link-done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
     return (
         <div>
-            <nav className='mt-8'>
-                <Link className='mx-6 hover:bg-blue-300 hover:text-white rounded-md p-3' to='/home'>HOME</Link>
-                <Link className='mx-6 hover:bg-blue-300 hover:text-white rounded-md p-3' to='/reviews'>REVIEWS</Link>
-                <Link className='mx-6 hover:bg-blue-300 hover:text-white rounded-md p-4' to='/dashboard'>DASHBOARD</Link>
-                <Link className='mx-6 hover:bg-blue-300 hover:text-white rounded-md p-3' to='/blogs'>BLOGS</Link>
+            <nav className='mt-8 flex justify-center'>
+                <CustomLink className='mx-6 hover:bg-blue-300 hover:text-white rounded-md p-3' to='/home'>HOME</CustomLink>
+                <CustomLink className='mx-6 hover:bg-blue-300 hover:text-white rounded-md p-3' to='/reviews'>REVIEWS</CustomLink>
+                <CustomLink className='mx-6 hover:bg-blue-300 hover:text-white rounded-md p-4' to='/dashboard'>DASHBOARD</CustomLink>
+                <CustomLink className='mx-6 hover:bg-blue-300 hover:text-white rounded-md p-3' to='/blogs'>BLOGS</CustomLink>
             </nav>
         </div>
     );
