7f722078b263d8dfb8dcff689e00a802851fbdeb
adritadeb <adritad2019@gmail.com>
2022-04-04 19:31:24 +0600

    second-QnA-done

---
+++ src/components/BlogAns2/BlogAns2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BlogAns2 = () => {
+    return (
+        <div>
+            <h2 className='text-2xl mb-4 mt-9'>2. What is Semantic tag?</h2>
+            <p className='mx-4'>-- The semantic tag describes how information looks on the UI. It contains many meaningful HTML tags. By using those tags, one can easily understand the purpose of the code. The browser can also easily understand the meaning of the information inside the tags. There are particular semantic tags for the particular sections of UI. For example - header tag, nav tag, section tag, aside tag, footer tag, etc.</p>
+        </div>
+    );
+};
+
+export default BlogAns2;
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import BlogAns1 from '../BlogAns1/BlogAns1';
+import BlogAns2 from '../BlogAns2/BlogAns2';
 
 const Blogs = () => {
     return (
         <div className='text-left m-16'>
-            <h1>Blog Section</h1>
+            <h1 className='text-4xl my-12 text-purple-600 font-semibold'>Blog Section</h1>
             <BlogAns1></BlogAns1>
+            <BlogAns2></BlogAns2>
         </div>
     );
 };
