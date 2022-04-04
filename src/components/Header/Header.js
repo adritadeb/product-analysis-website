6e37cf7c85db7349c1364ae43549cd1e55305047
adritadeb <adritad2019@gmail.com>
2022-04-04 19:58:23 +0600

    header-part-editted

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,9 @@ import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
     return (
-        <div>
-            <nav className='mt-8 flex justify-center'>
+        <div className='flex justify-between ml-14 mb-8 mt-5'>
+            <h1 className='text-3xl font-bold text-blue-400'>Skill Up</h1>
+            <nav className='flex justify-center'>
                 <CustomLink className='mx-2 md:mx-6 hover:bg-blue-300 hover:text-white rounded-md p-3' to='/home'>HOME</CustomLink>
                 <CustomLink className='mx-2 md:mx-6 hover:bg-blue-300 hover:text-white rounded-md p-3' to='/reviews'>REVIEWS</CustomLink>
                 <CustomLink className='mx-2 md:mx-6 hover:bg-blue-300 hover:text-white rounded-md p-4' to='/dashboard'>DASHBOARD</CustomLink>
